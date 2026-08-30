5cc77b15bba8466b068db6e276bcf70a184252a5
Saanvika Das <saanvikadas@MacBookAir.lan>
2026-08-30 11:48:05 +0200

    Fixed Reservations page

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -57,7 +57,7 @@ function ReservationPage() {
     return (
         <Container maxWidth="lg" sx={{ mt: 4, mb: 8 }}>
             <Button component={RouterLink} to={`/order/${slug}`} startIcon={<ArrowBackIcon />} sx={{ mb: 2 }}>
-                {t('backToMenu', { context: currentRestaurant?.businessType })}
+                {t('backToMenu', { context: restaurant?.businessType })}
             </Button>
             
             <Grid container spacing={4}>
